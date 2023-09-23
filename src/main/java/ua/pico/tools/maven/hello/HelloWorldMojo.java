5593df2fbd150a532654acd706c23e0bfe98271a
Vitalij Berdinskih <vitalij_r2@outlook.com>
2023-09-23 10:06:45 +0300

    Update project: dependencies, POM, Apache License instead of MIT, GitHub workflow etc.

--- src/main/java/ua/pico/tools/maven/hello/HelloWorldMojo.java
+++ src/main/java/ua/pico/tools/maven/hello/HelloWorldMojo.java
@@ -1,29 +1,6 @@
-/*
- * Copyright (c) 2013,2021 Witalij Berdinskich
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package ua.pico.tools.maven.hello;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -35,37 +12,37 @@ import org.apache.maven.project.MavenProject;
 @Mojo(name = "hello", defaultPhase = LifecyclePhase.VALIDATE, requiresProject = true)
 public class HelloWorldMojo extends AbstractMojo {
 
-	/**
-	 * Maven project
-	 */
-	@Parameter( defaultValue = "${project}", readonly = true )
-	private MavenProject project;
+  /**
+   * Maven project
+   */
+  @Parameter(defaultValue = "${project}", readonly = true)
+  private MavenProject project;
 
-	/**
-	 * Hello message.
-	 */
-	@Parameter(defaultValue = "Hello, world!", property = "hello.message")
-	private String message;
+  /**
+   * Hello message.
+   */
+  @Parameter(defaultValue = "Hello, world!", property = "hello.message")
+  private String message;
 
-	public void execute() {
-		getLog().info(message);
-		project.getProperties().put("hello.message", message);
-	}
+  public void execute() {
+    getLog().info(message);
+    project.getProperties().put("hello.message", message);
+  }
 
-	public String getMessage() {
-		return message;
-	}
+  public String getMessage() {
+    return message;
+  }
 
-	public void setMessage(final String message) {
-		this.message = message;
-	}
+  public void setMessage(final String message) {
+    this.message = message;
+  }
 
-	public MavenProject getProject() {
-		return project;
-	}
+  public MavenProject getProject() {
+    return project;
+  }
 
-	public void setProject(final MavenProject project) {
-		this.project = project;
-	}
+  public void setProject(final MavenProject project) {
+    this.project = project;
+  }
 
 }
--- src/test/java/ua/pico/tools/maven/hello/HelloWorldMojoTest.java
+++ src/test/java/ua/pico/tools/maven/hello/HelloWorldMojoTest.java
@@ -1,27 +1,11 @@
-/*
- * Copyright (c) 2013,2021 Witalij Berdinskich
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package ua.pico.tools.maven.hello;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Properties;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,44 +16,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 @ExtendWith(MockitoExtension.class)
 @Tag("fast")
 public class HelloWorldMojoTest {
 
-	private static final String HELLO_PROPERTY = "hello.message";
-	private static final String TEST_MESSAGE = "544553545F4D455353414745";
+  private static final String HELLO_PROPERTY = "hello.message";
+  private static final String TEST_MESSAGE = "544553545F4D455353414745";
 
-	@Mock
-	private Log log;
-	@Mock
-	private MavenProject project;
+  @Mock
+  private Log log;
+  @Mock
+  private MavenProject project;
 
-	@InjectMocks
-	private HelloWorldMojo testInstance;
+  @InjectMocks
+  private HelloWorldMojo testInstance;
 
-	private Properties projectProperties;
+  private Properties projectProperties;
 
-	@BeforeEach
-	public void setUp() {
-		projectProperties = new Properties();
-		testInstance.setMessage(TEST_MESSAGE);
+  @BeforeEach
+  public void setUp() {
+    projectProperties = new Properties();
+    testInstance.setMessage(TEST_MESSAGE);
 
-		when(project.getProperties()).thenReturn(projectProperties);
-	}
+    when(project.getProperties()).thenReturn(projectProperties);
+  }
 
-	@Test
-	public void testExecute() {
-		testInstance.execute();
+  @Test
+  public void testExecute() {
+    testInstance.execute();
 
-		verify(log).info(eq(TEST_MESSAGE));
-		assertEquals(TEST_MESSAGE, projectProperties.get(HELLO_PROPERTY));
-	}
+    verify(log).info(eq(TEST_MESSAGE));
+    assertEquals(TEST_MESSAGE, projectProperties.get(HELLO_PROPERTY));
+  }
 
 }
